cfe165b34ce6fde5e3215af1e50ab3fc27f0a523
James Newton <blackslate@lexogram.com>
2015-09-17 19:35:23 -0400

    Hello World as a graph on an interactive canvas

--- client/templates/main.js
+++ client/templates/main.js
@@ -1,13 +1,92 @@
 
+Template.main.onCreated(function() {
+  // Create objects which will be populated in main.onRendered
+  this.nodesDS = []   // will become new vis.DataSet([])
+  this.edgesDS = []   // will become new vis.DataSet([])
+  this.Network = null // will become new vis.Network(...)
+})
+
 Template.main.onRendered(function() {
-  var container = this.find('#graph')
+  var container     // reference to HTML <div id="graph">
+    , data          // { nodes: <DataSet>, edges: <DataSet> }
+    , fetchFunction // function fetchGraphData(), provided to 
+                    // Template.main, to be called each time the
+                    // template is rendered
+    , options       // object to hold canvas display options
 
-  Meteor.call('graph', graphCallback)
+  // Get HTML <div> where canvas is to appear
+  container = document.getElementById('graph')
 
-  function graphCallback(error, data) {
-    if (!error) {
-      // Data should be the string "Hello world"
-      container.innerHTML = data
-    }
+  // Initialize the vis object
+  this.nodesDS = new vis.DataSet([])
+  this.edgesDS = new vis.DataSet([])
+  data = {
+    nodes: this.nodesDS,
+    edges: this.edgesDS
   }
+  options = {} // Provide canvas display options here
+  this.Network = new vis.Network(container, data, options)
+  
+  // Call the server to get the data to display
+  fetchFunction = (function(template) {
+    // template will be the Blaze.Template instance for Template.main
+    return function fetchGraphData() {
+      Meteor.call('graph', graphCallback)
+
+      function graphCallback(error, data) {
+        // console.log(data)
+        // { nodes: [
+        //     { id:     <Neo4j id>
+        //     , labels: <Neo4j labels>
+        //     , label:  <Neo4j properties.name>
+        //     , group:  <Neo4j labels[0]>
+        //     , custom: <Neo4j properties.custom>
+        //     , ...
+        //     }
+        //   , ...
+        //   ]
+        // , edges: [
+        //     { id:     <Neo4j id>
+        //     , from:   <Neo4j startNode>
+        //     , to:     <Neo4j endNode>
+        //     , type:   <Neo4j type>
+        //     , label:  <Neo4j type
+        //     , arrows: "to"
+        //     , custom:  <Neo4j properties.custom>
+        //     , ...
+        //     }
+        //   , ...
+        //   ]
+        // }
+        
+        var edge
+          , ii
+          , count
+          , node
+          , dataNodes
+          , dataEdges
+
+        if (error) {
+          throw new Meteor.Error(error)
+
+        } else {
+          // Add the nodes and edges to the DataSets shared with the
+          // vis.Network for this template.
+          dataNodes = data.nodes
+          for (ii=0, count=dataNodes.length; ii < count; ii += 1) {
+            node = dataNodes[ii]
+            template.nodesDS.add([node]);                
+          }
+
+          dataEdges = data.edges
+          for (ii=0, count=dataEdges.length; ii < count; ii += 1) {
+            edge = dataEdges[ii]
+            template.edgesDS.add([edge]);          
+          }
+        }
+      }
+    }
+  })(this)
+
+  return fetchFunction()
 })
--- server/server.js
+++ server/server.js
@@ -9,64 +9,189 @@ Meteor.startup(function() {
     }
   )
 
-  Meteor.methods({
-    graph: function() {
-      var graph
-        , row
-        , edge
-        , startId
-        , endId
-        , nodes
-        , nodeNames
-        , output
+  ;(function ensureGraphIsPopulated(){
+    var result = db.queryOne('MATCH (n) RETURN n LIMIT 1')
+    // console.log("queryOne:",result)
+    // undefined or a mondo document { n: 
+    // { name: 'Amy',
+    //   id: 99,
+    //   labels: [ 'Person' ],
+    //   metadata: { id: 99, labels: [Object] } } }
 
-      // Make a synchronous call to the Neo4j database
+    if (!result) {
+      // The database is empty. Seed it with some random Persons who
+      // KNOWS each other.
+      result = db.querySync(
+        'WITH ["Amy","Bob","Cal"] AS names ' + // ,"Dan","Eve"]
+        'UNWIND names AS name ' +
+        'CREATE p = (:Person {name: name}) ' +
+        'WITH p ' +
+        'MATCH (p1:Person), (p2:Person) ' +
+        'WITH p1, p2 ' +
+        'WHERE rand() < 0.3 ' + // a random subset of possibilities
+        'AND p1 <> p2 ' +
+        'MERGE p1-[r:KNOWS]->p2 ' +
+        'RETURN DISTINCT p1, r, p2'
+      )
+      // console.log("querySync:",result)
+      // mongo cursor
+      // { _cursor: 
+      //   [ { p1: [Object], r: [Object], p2: [Object] },
+      //     ...
+      //     { p1: [Object], r: [Object], p2: [Object] } ],
+      //   length: 12,
+      //   _current: 0,
+      //   hasNext: true,
+      //   hasPrevious: false }
+    }
+  })()
+
+  return Meteor.methods({
+    graph: function() {
+      // The calls to the datbase is made synchronously, by not
+      // providing a callback. This means that we can treat the
+      // result immediately and return the treated data to the client
+      // caller via its callback.
+
+      var graph     // result from synchronous call to db.graph()
+        , visGraph  // output return via callback to the client
+                    // format: { nodes: [...], edges: [...]}
+        , nodes     // reference to visGraph.nodes
+        , edges     // reference to visGraph.edges
+        , nodeIds   // used to track which nodes have been treated
+        , edgeIds   // used to track which edges have been treated
+        , rowCount  // number of rows in graph
+        , itemCount // number of node or edge items in current row
+        , row       // current row from graph
+        , ii        // iterator for rows
+        , jj        // iterator for nodes and edges in current row
+        , id        // id of the current node or edge
+        , items     // array of node or edge items in current row
+        , neo4jItem // object: node or edge currently being treated
+        , visjsItem // object: neo4j data converted to visjs format
+
+      // Get the graph data from the database in Neo4j format
       graph = db.graph(
-        'MATCH ()-[r]-() ' +
-        'RETURN DISTINCT r'
+        'MATCH (n) ' +
+        'OPTIONAL MATCH ()-[r]-() ' +
+        'RETURN DISTINCT n, r'
       ).fetch()
 
-      console.log("graph:", JSON.stringify(graph))
+      // console.log("graph:", JSON.stringify(graph))
       // Array with a format like:
-      // [ { "relationships" : [
-      //       { "id":"287"
-      //       , "type":"LINK"
-      //       , "startNode":"158"
-      //       , "endNode":"159"
+      // [ ...
+      // , { "relationships": [
+      //       { "id":"266"
+      //       , "type":"LIKES"
+      //       , "startNode":"132"
+      //       , "endNode":"133"
       //       , "properties":{}
       //       }
       //     ]
       //   , "nodes": [
-      //       { "id":"158"
-      //       , "labels":[]
-      //       , "properties":{"name":"Hello"}
+      //       { "id":"131"
+      //       , "labels":["Person"]
+      //       , "properties":{"name":"Amy"}
       //       }
-      //     , { "id":"159"
-      //       , "labels":[]
-      //       , "properties":{"name":"World"}
+      //     , { "id":"132"
+      //       , "labels":["Person"]
+      //       , "properties":{"name":"Bob"}
+      //       }
+      //     , { "id":"133"
+      //       ,"labels":["Person"]
+      //       ,"properties":{"name":"Cal"}
       //       }
       //     ]
-      // } ]
-      
-      // Get the first row of the graph and find the start and end
-      // of the first relationship
-      row = graph[0]
+      //   }
+      // , ...]
+      // NOTES:
+      // 1. There may be more than 2 nodes in the "nodes" section,
+      //    but each relationship in the "relationships" section links
+      //    only the given startNode and endNode.
+      // 2. The same relationship may appear multiple times, sometimes
+      //    indentically, sometimes with a different set of nodes. We
+      //    must filter out duplicates.
 
-      edge = row.relationships[0]
-      startId = edge.startNode || edge.start
-      endId = edge.endNode || edge.endNode
+      // Convert to visjs format:
+      // { nodes: [
+      //     { id:     <Neo4j id>
+      //     , labels: <Neo4j labels>
+      //     , label:  <Neo4j properties.name>
+      //     , group:  <Neo4j labels[0]>
+      //     , custom: <Neo4j properties.custom>
+      //     , ...
+      //     }
+      //   , ...
+      //   ]
+      // , edges: [
+      //     { id:     <Neo4j id>
+      //     , from:   <Neo4j startNode>
+      //     , to:     <Neo4j endNode>
+      //     , type:   <Neo4j type>
+      //     , label:  <Neo4j type
+      //     , arrows: "to"
+      //     , custom:  <Neo4j properties.custom>
+      //     , ...
+      //     }
+      //   , ...
+      //   ]
+      // }
 
-      // Get the names of all the nodes in this row
-      nodes = row.nodes
-      nodeNames = {}
-      for (var ii = 0, node; node = nodes[ii]; ii += 1 ) {
-        nodeNames[node.id] = node.properties.name
+     nodes = []
+      edges = []
+      visGraph = {
+        nodes: nodes,
+        edges: edges
+      }
+      // helper arrays to hold ids of treated items
+      nodeIds = []
+      edgeIds = []
+
+      for (ii = 0, rowCount = graph.length; ii < rowCount; ii += 1) {
+        row = graph[ii]
+
+        items = row.nodes
+        itemCount = items.length         
+        for (jj = 0; jj < itemCount; jj += 1) {
+          neo4jItem = items[jj]
+          id = neo4jItem.id
+          if (nodeIds.indexOf(id) < 0) {
+            // This node has not been treated yet. Remember it.
+            visjsItem = {
+              id: id
+            , labels: neo4jItem.labels
+            , label: neo4jItem.properties.name
+            , group: neo4jItem.labels[0]
+            }
+            visjsItem = _.extend(visjsItem, neo4jItem.properties)
+            nodes.push(visjsItem)
+            nodeIds.push(id) // Don't treat it again
+          }
+        }
+
+        items = row.relationships
+        itemCount = items.length          
+        for (jj = 0; jj < itemCount; jj += 1) {
+          neo4jItem = items[jj]
+          id = neo4jItem.id
+          if (edgeIds.indexOf(id) < 0) {
+            // This edge has not been treated yet. Remember it.
+            visjsItem = {
+              id: id
+            , from: neo4jItem.startNode || neo4jItem.start
+            , to: neo4jItem.endNode || neo4jItem.end
+            , type: neo4jItem.type
+            , label: neo4jItem.type
+            , arrows: 'to'
+            }
+            visjsItem = _.extend(visjsItem, neo4jItem.properties)
+            edges.push(visjsItem)
+            edgeIds.push(id) // Don't treat it again
+          }
+        }
       }
 
-      // Create a string from the names of the start and end nodes
-      output = nodeNames[startId] + " " + nodeNames[endId]
-
-      return output
+      return visGraph
     }
   })
 })
